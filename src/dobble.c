8a9cadaa65f3c626e54b9b14291207a17f3b02f7
sebastian fougere <sebastian.fougere@gmail.com>
2018-12-04 11:43:07 +0100

    Timer et Score

--- src/dobble.c
+++ src/dobble.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include <SDL2/SDL.h>
 
 #include "dobble-config.h"
@@ -11,7 +11,8 @@
 
 /// Etat du compte à rebous (lancé/non lancé)
 static bool timerRunning = false;
-
+static int score = 0;
+static int timer = 0;
 
 void onMouseMove(int x, int y)
 {
@@ -26,13 +27,14 @@ void onMouseClick()
 
 	if (timerRunning)
 	{
-		printf("\ndobble: Arrêt du compte à rebours.\n");
-		stopTimer();
-		timerRunning = false;
+		if (1==1){if (1==1){timer+=3;
+score+=1;}else{timer-=3;}
+}else{printf("Vous devez cliquer sur une icone");}
 	}
 	else
 	{
 		printf("\ndobble: Démarrage du compte à rebours.\n");
+		timer=3;
 		startTimer();
 		timerRunning = true;
 	}
@@ -40,7 +42,13 @@ void onMouseClick()
 
 void onTimerTick()
 {
+	timer-=1;
+	if (timer<=0){
+stopTimer();
+printf("Fin du temps");}
 	printf("\ndobble: Tic du compte à rebours\n");
+	renderScene();
+	
 }
 
 
@@ -51,15 +59,17 @@ void renderScene()
 	// Efface le contenu de la fenêtre
 	clearWindow();
 	// Crée le titre qui sera affiché. Utile pour afficher le score.
-	sprintf(title, "RICM3-Dobble    Score %d", 100);
+	sprintf(title, "RICM3-Dobble    Score %d", score);
 	drawText(title, WIN_WIDTH / 2, 0, Center, Top);
+	sprintf(title,"Temps %d", timer);
+	drawText(title, WIN_WIDTH / 2, 25, Center, Top);
 
 	CardPosition currentCard = UpperCard;
 	double radius = CARD_RADIUS * 0.6;
 	double angle;
 	double rotation;
 	double scale = 1.;
-	int icon = 0;
+	int icon;
 	int cx, cy;
 
 	// Dessin du fond de carte de la carte supérieure
@@ -69,7 +79,7 @@ void renderScene()
 	for (angle = 0.; angle < 360.; angle += 360. / 7.)
 	{
 		rotation = sin(angle) * angle + 120.;
-
+		icon=rand()%8;
 		drawIcon(currentCard, icon, radius, angle, rotation, scale, &cx, &cy);
 
 		// (cx, cy) est le centre de l'icône placé à l'écran (en pixels)
@@ -100,7 +110,7 @@ void renderScene()
 
 int main(int argc, char **argv)
 {
-
+	srand(time(NULL));
 	if (!initializeGraphics())
 	{
 		printf("dobble: Echec de l'initialisation de la librairie graphique.\n");
