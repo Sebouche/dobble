f548b39627fc9a1d81fba94824de3af55079d44d
sebastian fougere <sebastian.fougere@gmail.com>
2018-12-12 16:10:43 +0100

    tmp

--- src/dobble.c
+++ src/dobble.c
@@ -12,21 +12,38 @@
 
 /// Etat du compte à rebous (lancé/non lancé)
 static bool timerRunning = false;
+
+//Score et temps et difficulté choisie (Facile de base)
 static int score = 0;
 static int timer = 0;
+int difficulte = 1;
+
+//Informations sur les cartes
+int Nbicons=7;
 static bool trouve = true;
 double rotations[16];
 double scales[16];
+int radiuss[16];
 int icons[16];
+int indiceimage=0;
+
+//Informations pour le menu
 int posX;
 int posY;
 char *Options[4];
 int Nboptions;
 int positionactuelle = -1;
-int difficulte = 1;
 SDL_Rect rectangles[4];
 SDL_Point Souris;
 SDL_Texture *TextureImage;
+
+//Informations pour la musique
+SDL_AudioSpec audioSortie;
+Uint32 audioLen, audioPos;
+SDL_AudioSpec audioBufferSpec;
+Uint8 *audioBuffer;
+Uint32 audioBufferLen;
+
 
 //initialise le menu avec les options choisies
 void initMenu(char **parametres, int Nb)
@@ -94,26 +111,28 @@ void onMouseClick()
 		int cardCenterX, cardCenterY;
 		getCardCenter(UpperCard, &cardCenterX, &cardCenterY);
 		if (posX >=
-		    CARD_RADIUS * 0.6 * cos(0. / 360. * (2. * M_PI)) +
+		    CARD_RADIUS * 0.6 * cos(indiceimage / 360. * (2. * M_PI)) +
 		    cardCenterX - 45
 		    && posX <=
-		    CARD_RADIUS * 0.6 * cos(0. / 360. * (2. * M_PI)) +
+		    CARD_RADIUS * 0.6 * cos(indiceimage / 360. * (2. * M_PI)) +
 		    cardCenterX + 45
 		    && posY >=
-		    CARD_RADIUS * 0.6 * sin(0. / 360. * (2. * M_PI)) +
+		    CARD_RADIUS * 0.6 * sin(indiceimage / 360. * (2. * M_PI)) +
 		    cardCenterY - 45
 		    && posY <=
-		    CARD_RADIUS * 0.6 * sin(0. / 360. * (2. * M_PI)) +
+		    CARD_RADIUS * 0.6 * sin(indiceimage / 360. * (2. * M_PI)) +
 		    cardCenterY + 45) {
 			trouve = true;
 			timer += 3;
 			score += 1;
+			renderScene();
 		} else {
 			timer -= 3;
 			if (timer <= 0) {
 				stopTimer();
 				printf("Fin du temps");
 			}
+			else{renderScene();}
 		}
 	} else {
 		printf
@@ -122,16 +141,18 @@ void onMouseClick()
 
 }
 
-void onTimerTick()
+//On decremente le temps toute les secondes et on met a jour l'affichage, si le temps est <=0 on arrete le timer et on renvoi 1 pour indiquer qu'il faut arreter la boucle
+int onTimerTick()
 {
 	timer -= 1;
 	if (timer <= 0) {
 		stopTimer();
 		printf("Fin du temps");
+		return 1;
 	}
 	printf("\ndobble: Tic du compte à rebours\n");
 	renderScene();
-
+	return 0;
 }
 
 //Renvoi l'indice de l'image en commun sur la carte superieure 
@@ -156,25 +177,30 @@ void renderScene()
 	// Crée le titre qui sera affiché. Utile pour afficher le score.
 	sprintf(title, "RICM3-Dobble    Score %d", score);
 	drawText(title, WIN_WIDTH / 2, 0, Center, Top);
-	sprintf(title, "Temps %d", timer);
+	sprintf(title, "Temps restant %d", timer);
 	drawText(title, WIN_WIDTH / 2, 25, Center, Top);
-	double radius = CARD_RADIUS * 0.6;
+	//Crée les informations necessaires pour la creation des cartes
+	double radius ;
 	double angle;
 	double rotation;
-	double scale = 1.;
+	double scale;
 	int icon;
 	int cx, cy;
 	int i = 0;
+	//Si on commence la partie ou que le joueur a trouvé le symbole en commun on genere les cartes
 	if (trouve) {
 		CardPosition currentCard = UpperCard;
 		// Dessin du fond de carte de la carte supérieure
 		drawCardShape(currentCard, 5, 252, 252, 252, 155, 119, 170);
 
 		// Affichage des icônes de la carte du haut (régulièrement en cercle)
-		for (angle = 0.; angle < 360.; angle += 360. / 7.) {
-			//scale = (double)(rand() % 5 + 1) / 5;
+		for (angle = 0.; angle < 360.; angle += 360. / Nbicons) {
+			radius=CARD_RADIUS * ((30+rand()%40)/100);
+			scale = radius/(CARD_RADIUS*0.6);
+			printf("%f\n",radius);
 			rotation = rand() % 360;
 			icon = rand() % 80;
+			radiuss[i]=radius;
 			rotations[i] = rotation;
 			scales[i] = scale;
 			icons[i] = icon;
@@ -184,9 +210,11 @@ void renderScene()
 
 			// (cx, cy) est le centre de l'icône placé à l'écran (en pixels)
 		}
-		//scale = (double)(rand() % 5 + 1) / 5;
+		radius=CARD_RADIUS * ((100-rand()%50)/100);
+		scale = radius/(CARD_RADIUS*0.6);
 		icon = rand() % 80;
 		rotation = rand() % 360;
+		radiuss[i]=radius;
 		rotations[i] = rotation;
 		scales[i] = scale;
 		icons[i] = icon;
@@ -201,11 +229,13 @@ void renderScene()
 		drawCardShape(currentCard, 5, 252, 252, 252, 155, 119, 170);
 
 		// Affichage des icônes de la carte du bas (régulièrement en cercle)
-		for (angle = 0.; angle < 360.; angle += 360. / 7.) {
-			//scale = (double)(rand() % 5 + 1) / 5;
+		for (angle = 0.; angle < 360.; angle += 360. / Nbicons) {
+			radius=CARD_RADIUS * ((100-rand()%50)/100);
+			scale = radius/(CARD_RADIUS*0.6);
 			rotation = rand() % 360;
 			icon = rand() % 80;
 			rotation = sin(angle) * angle + 70.;
+			radiuss[i]=radius;
 			rotations[i] = rotation;
 			scales[i] = scale;
 			icons[i] = icon;
@@ -213,9 +243,11 @@ void renderScene()
 			drawIcon(currentCard, icon, radius, angle, rotation,
 				 scale, NULL, NULL);
 		}
-		//scale = (double)(rand() % 5 + 1) / 5;
+		radius=CARD_RADIUS * ((100-rand()%50)/100);
+		scale = radius/(CARD_RADIUS*0.6);
 		rotation = rand() % 360;
 		icon = rand() % 80;
+		radiuss[i]=radius;
 		rotations[i] = rotation;
 		scales[i] = scale;
 		icons[i] = icon;
@@ -227,10 +259,10 @@ void renderScene()
 		int j = 0;
 		drawCardShape(UpperCard, 5, 252, 252, 252, 155, 119, 170);
 		drawCardShape(LowerCard, 5, 252, 252, 252, 155, 119, 170);
-		for (angle = 0.; angle < 360.; angle += 360. / 7.) {
-			drawIcon(UpperCard, icons[j], radius, angle,
+		for (angle = 0.; angle < 360.; angle += 360. / Nbicons) {
+			drawIcon(UpperCard, icons[j], radiuss[j], angle,
 				 rotations[j], scales[j], &cx, &cy);
-			drawIcon(LowerCard, icons[j + 8], radius, angle,
+			drawIcon(LowerCard, icons[j + 8], radiuss[j+8], angle,
 				 rotations[j + 8], scales[j + 8], &cx, &cy);
 			j++;
 		}
@@ -249,6 +281,9 @@ void printstat()
 	char title[100];
 	// Efface le contenu de la fenêtre
 	clearWindow();
+	RenderImage(TextureImage);
+	sprintf(title, "Cliquez pour retourner au menu");
+	drawText(title, WIN_WIDTH / 2, WIN_HEIGHT/2 -60, Center, Top);
 	sprintf(title, "Facile");
 	drawText(title, WIN_WIDTH / 2 - 80, WIN_HEIGHT / 2, Center, Top);
 	sprintf(title, "Normal");
@@ -290,11 +325,13 @@ void printresultat()
 	// Efface le contenu de la fenêtre
 	clearWindow();
 	sprintf(title, "Votre score est : ");
-	drawText(title, WIN_WIDTH / 2, 0, Center, Top);
+	drawText(title, WIN_WIDTH / 2, WIN_HEIGHT/2, Center, Top);
 	sprintf(title, "%d", score);
-	drawText(title, WIN_WIDTH / 2, 25, Center, Top);
+	drawText(title, WIN_WIDTH / 2, WIN_HEIGHT/2+25, Center, Top);
 	sprintf(title, "Bravo!");
-	drawText(title, WIN_WIDTH / 2, 50, Center, Top);
+	drawText(title, WIN_WIDTH / 2, WIN_HEIGHT/2+50, Center, Top);
+	sprintf(title, "Cliquez pour retourner au menu");
+	drawText(title, WIN_WIDTH / 2,WIN_HEIGHT/2+ 70, Center, Top);
 	showWindow();
 }
 
@@ -325,6 +362,14 @@ void menuLoop()
 				mainLoop();
 				printresultat();
 				sauvegarder();
+				while (!quit2) {
+					SDL_WaitEvent(&event);
+					if (event.type == SDL_MOUSEBUTTONDOWN) {
+						quit2 = 1;
+					}
+				}
+				quit2 = 0;
+				renderMenu();
 				break;
 			case 1:
 				initMenu(parametres2, 3);
@@ -366,6 +411,7 @@ void menuLoop()
 					}
 				}
 				quit2 = 0;
+				renderMenu();
 				break;
 			case 3:
 				quit = 1;
@@ -386,21 +432,15 @@ void menuLoop()
 	}
 }
 
-SDL_AudioSpec audioSortie;
-Uint32 audioLen, audioPos;
-SDL_AudioSpec audioBufferSpec;
-Uint8 *audioBuffer;
-Uint32 audioBufferLen;
 
 void audioCallBack(void *udata, Uint8 * stream, int len)
 {
-	// On ne lit que s'il reste des données à jouer
+	// On ne lit que s'il y a des données à jouer
 	if (audioBufferLen == 0)
 		return;
 
 	// Remise à zéro du tampon de sortie
 	memset(stream, 0, len);
-
 	// Lecture du buffer audio
 	if (audioPos < audioBufferLen) {
 		if (audioPos + len > audioBufferLen)
@@ -462,12 +502,11 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	char title[100];
-	sprintf(title,DATA_DIRECTORY "/%s",argv[1]);
-	printf("%s",title);
-	FILE* f = fopen( title,"r"); // ouvre le fichier “fichier.txt” en lecture
-if (f == NULL) {
+	sprintf(title, DATA_DIRECTORY "/%s", argv[1]);
+	FILE *f = fopen(title, "r");	// ouvre le fichier “fichier.txt” en lecture
+	if (f == NULL) {
 // erreur d’ouverture
-}
+	}
 	SDL_PauseAudio(0);
 	menuLoop();
 	freeGraphics();
